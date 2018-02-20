7824bcbb8042d5e71a0d1647f22e774c2991aa46
Aitor <aitorreulamartinez@gmail.com>
2018-02-20 11:21:51 +0100

    clase gestión creada

---
+++ src/main/java/facturas/Tarifa.java
@@ -0,0 +1,20 @@
+package facturas;
+
+public class Tarifa {
+
+    private float precio;
+
+    public Tarifa(){
+        super();
+    }
+
+    public Tarifa(Tarifa tarifa){
+
+        this.precio= precio;
+    }
+
+    public float getPrecio(){
+
+        return precio;
+    }
+}
